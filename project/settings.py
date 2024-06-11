348714a8a5822301f1472d6982ab823bbae67e67
Masa Takeuchi <masahitoart@gmail.com>
2024-06-11 21:27:16 +0200

    uncomment dj_database

--- project/settings.py
+++ project/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 # from dotenv import load_dotenv
-import dj_database_url
+# import dj_database_url
 # load_dotenv()
 
 
